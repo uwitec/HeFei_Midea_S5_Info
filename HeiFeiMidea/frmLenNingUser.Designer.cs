4cee41997ac6828bf8bbd0fa7d58b6b418f4132c
帅 <312637477@qq.com>
2016-05-29 10:30:04 +0800

    2016-6-29

---
+++ HeiFeiMidea/frmLenNingUser.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace HeiFeiMidea
+{
+    partial class frmLenNingUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmLenNingUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 540);
+            this.Name = "frmLenNingUser";
+            this.Text = "冷凝器用户权限设置";
+            this.Load += new System.EventHandler(this.frmLenNingUser_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
